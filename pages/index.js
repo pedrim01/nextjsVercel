9d23ed69b8e69ee08849ded20f007d8d0f1e690c
pedrim01 <peuu.aguiar@gmail.com>
2023-01-23 17:44:08 -0300

    api greyhound com cache na vercel de 10segundos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-export async function getStaticProps(context) {
+export async function getStaticProps(request, response) {
   try {
     const res = await fetch(
       'https://greyhoundbet.racingpost.com/card/blocks.sd?race_id=1955873&r_date=2023-01-23&tab=form&blocks=card-header%2Ccard-pager%2Ccard-tabs%2Ccard-title%2Cform',
     )
     const posts = await res.json()
-    
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
     return {
       props: {
         posts,
