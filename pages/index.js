fe23adb7fc8df3c22090552834deb5083652ecef
pedrim01 <peuu.aguiar@gmail.com>
2023-01-23 17:33:26 -0300

    acesso a api da greyhound racing post no front usando getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
-
-
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-     
-    </div>
-  )
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch(
+      'https://greyhoundbet.racingpost.com/card/blocks.sd?race_id=1955873&r_date=2023-01-23&tab=form&blocks=card-header%2Ccard-pager%2Ccard-tabs%2Ccard-title%2Cform',
+    )
+    const posts = await res.json()
+    
+    return {
+      props: {
+        posts,
+      },
+    }
+  } catch (error) {
+    console.log(error)
+    return error
+  }
 }
 
-
-
-export default Home
+export default function Todos({ posts }) {
+  return (
+    <>
+      <ul>
+        {posts['form'].dogs.map((post) => (
+          <li>{post.dogName}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
