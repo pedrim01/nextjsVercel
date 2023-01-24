5cdeab7cb4c814d21dc80836948521717cac0c9a
pedrim01 <peuu.aguiar@gmail.com>
2023-01-23 22:46:40 -0300

    api tempo com pasta api criada com cache-control

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,8 @@
 function tempo (req, res){
 const dynamicDate = new Date()
 
+res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
+
 res.json ({
     date: dynamicDate.toGMTString()
 
